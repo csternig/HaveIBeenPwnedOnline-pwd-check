4ce70874ba5af6adf540224cca3ab317feca8085
csternig <christian@sternig.net>
2019-03-23 21:02:09 +0100

    added code documentation, removed while loop

--- online_search.py
+++ online_search.py
@@ -5,18 +5,26 @@ import hashlib
 import requests
 
 if __name__ == "__main__":
-    while True:
-        test_password = getpass.getpass('Enter password:')
-        if not test_password:
-            break
-        test_hash = hashlib.sha1(test_password.encode()).hexdigest().upper()
-        test_prefix = test_hash[:5]
-        print("Transmit hashed value over Internet: {}".format(test_prefix))
-        result = requests.get('https://api.pwnedpasswords.com/range/{}'.format(test_prefix))
-        num_hits = 0
+        print("Please enter a password for testing against the Have I Been Pawned database.")
+        print("Use no password an just press ENTER to exit the program.\n")
+        # reading a password from prompt in a secure way
+        password_console = getpass.getpass('Enter password:')
+        # creating a SHA1 hash of the password and reformat it for HIBP upload
+        password_hash = hashlib.sha1(password_console.encode()).hexdigest().upper()
+        print("Your local SHA1 password hash: {}".format(password_hash))
+        # using only the first 5 digits of the password hash to query the Internet database
+        password_prefix = password_hash[:5]
+        print("Transmit hashed value over Internet: {}".format(password_prefix))
+        # querying the results, a list of password SHA1 hash values
+        result = requests.get('https://api.pwnedpasswords.com/range/{}'.format(password_prefix))
+        num_hits = 0  # introducing a hit count variable
         for line in result.text.split('\r\n'):
-            partial_hash, count = line.split(':')
-            if test_hash[5:] == partial_hash:
+            # Have i been pawned returns a partial password hash (all digits except the first 5 and a count value
+            # separated by ':' for every possible hit
+            partial_hash, count = line.split(':')  # type: (str, int)
+            # check if tha password_hash (local value) matches the partial_hash (remote value from database)
+            if password_hash[5:] == partial_hash:
+                # if there's a match num_hits is updated with the count value and the for loop is left
                 num_hits = count
                 break
         print("The password has been found {} times".format(num_hits))
